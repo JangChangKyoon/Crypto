88fd95753f009a865dc5901c528abc61bf497edc
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-20 19:36:40 +0900

    extend style

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ const Box = styled.div`
   height: 100px;
 `;
 
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
   return (
     <Father>
       <Box bgColor="teal" />
-      <Box bgColor="tomato" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
