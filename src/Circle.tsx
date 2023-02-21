fc1315a464bfdde5d06307dd2ab47e8a2af80a78
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-21 17:23:45 +0900

    styled component type

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 import React from "react";
 
-const Container = styled.div``;
+interface ContainProps {
+  bgColor: string;
+}
+
+const Container = styled.div<ContainProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+`;
 
 interface CircleProps {
   bgColor: string;
