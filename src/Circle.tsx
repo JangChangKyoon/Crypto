16d64d9bdaf56000c2b3e1628d1a86f3b0155f91
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-21 17:07:26 +0900

    component

---
+++ src/Circle.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import React from "react";
+
+const Container = styled.div``;
+
+function Circle() {
+  return <Container />;
+}
+
+export default Circle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
 
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "black",
-};
-
-const lightTheme = {
-  textColor: "black",
-  backgroundColor: "whitesmoke",
-};
-
-ReactDOM.render(
-  <ThemeProvider theme={darkTheme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
