f0e22f819ecf9cc851a9af0a901355378e81b629
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-21 17:58:10 +0900

    optional type

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,20 +1,32 @@
 import styled from "styled-components";
 import React from "react";
 
-interface ContainProps {
+interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
-const Container = styled.div<ContainProps>`
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: ContainProps) {
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+}
+
+function Circle({ bgColor, borderColor }: CircleProps) {
   // = Circle({ bgColor }: CircleProps = props.bgColor : CircleProps
-  return <Container bgColor={bgColor} />;
+  return (
+    <Container
+      bgColor={bgColor}
+      borderColor={borderColor ?? bgColor}
+    ></Container>
+  );
 }
 
 export default Circle;
