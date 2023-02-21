c97fa22b7c336d159850bb492ba59e96ef4360c4
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-22 00:06:10 +0900

    초기화

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log In</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
