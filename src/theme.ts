e0862ff340a9234a3037780bfa8d15705f0c8a3d
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-22 21:57:57 +0900

    code arrangement

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
-export const ligntTheme: DefaultTheme = {
+export const theme: DefaultTheme = {
   bgColor: "white",
   textColor: "black",
   btnColor: "tomato",
-};
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "teal",
 };
