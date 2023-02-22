da3388f9cfe7a1d751e17c266f93d4f20d2db992
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-22 23:03:25 +0900

    code arrangement

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
 
 function App() {
-  return (
-    <div>
-      <Container>
-        <H1>protected</H1>
-      </Container>
-    </div>
-  );
+  return null;
 }
 
 export default App;
