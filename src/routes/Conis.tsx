147e4c53b5a7d9b31f3f8496225ccc7073c0078f
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-23 22:11:56 +0900

    loading render before api fetched

--- src/routes/Conis.tsx
+++ src/routes/Conis.tsx
@@ -37,6 +37,11 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
 interface CoinInterface {
   id: string;
   name: string;
@@ -55,7 +60,7 @@ function Coins() {
       const response = await fetch("https://api.coinpaprika.com/v1/coins");
       const json = await response.json();
       setCoins(json.slice(0, 100)); // 100개까지만 불러옴
-      console.log(json);
+      // console.log(json);
       setLoading(false);
     })();
   }, []);
@@ -64,13 +69,17 @@ function Coins() {
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 }
